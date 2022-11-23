97bac8e1b5687714a6182ca3e99ebb13c4cbd94c
dariocond <66563814+dariocon@users.noreply.github.com>
2022-11-23 10:04:31 +0100

    Update modular2ejercicios.py

--- ProgramacionModular2/modular2ejercicios.py
+++ ProgramacionModular2/modular2ejercicios.py
@@ -3,4 +3,12 @@ Created on Nov 22, 2022
 
 @author: estudiante
 '''
-fg
+
+"""1."""
+numero=4
+def factorial(numero):
+    factorial=1
+    for j in range(1,numero+1):
+            factorial=factorial*j
+    return factorial
+print(factorial(numero))
